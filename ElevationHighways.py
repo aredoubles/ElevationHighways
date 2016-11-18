e2526d11ffc0390d7fbbaae0e528d4e7f07fe155
aredoubles <roger.w.shaw@gmail.com>
2016-11-18 18:06:12 -0500

    I-70 add

--- ElevationHighways.py
+++ ElevationHighways.py
@@ -1,6 +1,13 @@
 # Charting elevations of every major US Highway
 # Using Google Maps APIs
 
+'''
+TODO:
+I-70
+I-90
+I-64?
+'''
+
 import googlemaps
 import json
 import matplotlib.pyplot as plt
@@ -24,8 +31,26 @@ def Routing(highway):
     # Parse this KML/XML, so that it can be treated like a JSON
     doc = xmltodict.parse(fileobject)
     # Coordinates are basically a single enormous string. Need to split by spaces (waypoints), and commas (lat/lon)
+    rawcoords = doc['kml']['Document']['Placemark']['LineString']['coordinates']
+    listcoords = rawcoords.split(' ')
+    newcoords = []
+    for pair in listcoords:
+        pair = pair.split(',')
+        pair = [float(pair[1]), float(pair[0])]     # Flip lat/lon presentation
+        newcoords.extend(pair)
+    coords = zip(newcoords[::2],newcoords[1::2])    # Pairs of records are zipped together to form tuples
+    '''
     for chunk in doc['kml']['Document']['Placemark']:
-        if chunk['name'].startswith('Interstate'):
+        if doc['kml']['Document']['Placemark'].startswith('Driving'):
+            rawcoords = doc['kml']['Document']['Placemark']['LineString']['coordinates']
+            listcoords = rawcoords.split(' ')
+            newcoords = []
+            for pair in listcoords:
+                pair = pair.split(',')
+                pair = [float(pair[1]), float(pair[0])]     # Flip lat/lon presentation
+                newcoords.extend(pair)
+            coords = zip(newcoords[::2],newcoords[1::2])    # Pairs of records are zipped together to form tuples
+        elif chunk['name'].startswith('Interstate'):
             rawcoords = chunk['LineString']['coordinates']
             listcoords = rawcoords.split(' ')
             newcoords = []
@@ -34,7 +59,7 @@ def Routing(highway):
                 pair = [float(pair[1]), float(pair[0])]     # Flip lat/lon presentation
                 newcoords.extend(pair)
             coords = zip(newcoords[::2],newcoords[1::2])    # Pairs of records are zipped together to form tuples
-        if chunk['name'].startswith('Driving'):
+        elif chunk['name'].startswith('Driving'):
             rawcoords = chunk['LineString']['coordinates']
             listcoords = rawcoords.split('        ')
             newcoords = []
@@ -43,6 +68,7 @@ def Routing(highway):
                 pair = [float(pair[1]), float(pair[0])]     # Flip lat/lon presentation
                 newcoords.extend(pair)
             coords = zip(newcoords[::2],newcoords[1::2])    # Pairs of records are zipped together to form tuples
+    '''
     # len(coords) -> 7645 coordinates, might be too much for the API to handle at once? Getting an HTTP error: 413
     # Error message says max waypoints: 23.   7625/23 = 332.391304348
     waynm = int(len(coords) / 20)
